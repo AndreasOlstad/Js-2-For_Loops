d132669254413a697e85bb2011dfb8997ccab43b
AndreasOlstad <andreasolstad2007@gmail.com>
2024-02-06 10:27:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+/*Oppgave 1, viser tallene 1-50
+for (let i = 0; i <= 50; i++) {
+    console.log (i)
+    
+}
+*/
+
+/* Oppgave 2, viser partallene 1-50
+for (let i = 0; i <= 50; i = i+2) {
+    console.log (i)
+    
+}
+*/
+
+/*Oppgave 3, viser tall fra 1-100 som er delbare med både 3 og 5
+for (let i = 0; i <= 100; i++) {
+    if (i%3 == 0 && i%5 == 0)
+    console.log (i)
+}
+*/
+
+//Oppgave 4, viser tall som bare er delbare med 3 eller 5
+/*
+for (let i = 0; i <= 100; i++) {
+    if (i%3 == 0 || i%5 == 0)
+    console.log (i)
+}
+*/
+
+//Utfordring 1
+/*
+for (let i = 6; i > 0; i--) {
+    document.write (`<h${i}> Røyken Videregående Skole </h${i}>`)
+}
+*/
+
+//Utfordring 2
+/*
+for (let i = 1; i <= 5; i++) {
+    for (let a = 1; a <= 8; a++) {
+       if (i == 1 || i == 5 || a == 1 || a == 8){
+        document.write("*")
+       }
+       else{
+        document.write("&nbsp;&nbsp;")
+       }
+    }
+    document.write(`<br>`)
+}
+*/
